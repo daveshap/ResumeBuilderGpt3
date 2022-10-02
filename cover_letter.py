98d552f567656fffd021be7169993e93f5e0fb71
David Shapiro <daveshap37@gmail.com>
2022-10-02 07:59:13 -0400

    starting work

---
+++ cover_letter.py
@@ -0,0 +1,55 @@
+import re
+import json
+import openai
+from time import time,sleep
+
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+
+def save_file(filepath, content):
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        outfile.write(content)
+
+
+def load_info():
+    info = open_file('my_info.json')
+    info = json.loads(definitions)
+    return info
+
+
+openai.api_key = open_file('openaiapikey.txt')
+
+
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['asdfasdf', 'asdasdf']):
+    max_retry = 5
+    retry = 0
+    prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()  # force it to fix any unicode errors
+    while True:
+        try:
+            response = openai.Completion.create(
+                engine=engine,
+                prompt=prompt,
+                temperature=temp,
+                max_tokens=tokens,
+                top_p=top_p,
+                frequency_penalty=freq_pen,
+                presence_penalty=pres_pen,
+                stop=stop)
+            text = response['choices'][0]['text'].strip()
+            #text = re.sub('\s+', ' ', text)
+            filename = '%s_gpt3.txt' % time()
+            save_file('gpt3_logs/%s' % filename, prompt + '\n\n==========\n\n' + text)
+            return text
+        except Exception as oops:
+            retry += 1
+            if retry >= max_retry:
+                return "GPT3 error: %s" % oops
+            print('Error communicating with OpenAI:', oops)
+            sleep(1)
+
+
+if __name__ == '__main__':
+    
